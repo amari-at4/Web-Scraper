ab9ad8719e6a2b6761719e0c72a7dab83b397857
Antonio Marí <amari@at4.net>
2021-09-29 22:10:10 +0200

    Stage 4/5

--- Web Scraper/task/scraper.py
+++ Web Scraper/task/scraper.py
@@ -1,18 +1,45 @@
 import requests
+import string
+from bs4 import BeautifulSoup
 
 
 class WebScrapper:
     @staticmethod
-    def save_content_to_file(url):
-        print()
+    def scrape_url():
+        url = 'https://www.nature.com/nature/articles?sort=PubDate&year=2020&page=3'
         response = requests.get(url, headers={'Accept-Language': 'en-US,en;q=0.5'})
         if response:
-            with open('source.html', 'wb') as source_file:
-                source_file.write(response.content)
-                print('Content saved.')
-        else:
-            print(f"The URL returned {response.status_code}")
+            movie = dict()
+            main_html_parser = HtmlParser(response.content)
+            articles = []
+            for article in main_html_parser.get_news_article():
+                anchor = article.find('h3', 'c-card__title').find('a')
+                href = f"https://www.nature.com{anchor.get('href')}"
+                sanitized_title = "_".join(anchor.string.translate(str.maketrans('', '', string.punctuation)).split())
+                filename = f"{sanitized_title}.txt"
+                response = requests.get(href, headers={'Accept-Language': 'en-US,en;q=0.5'})
+                if response:
+                    article_html_parser = HtmlParser(response.content)
+                    with open(filename, "w", encoding="UTF-8") as article_file:
+                        article_file.write(article_html_parser.get_article_body())
+                        articles.append(filename)
+            print("Saved articles:", articles)
 
 
-print("Input the URL:")
-WebScrapper.save_content_to_file(input())
+class HtmlParser:
+    def __init__(self, html_content):
+        self.parser = BeautifulSoup(html_content, 'html.parser')
+
+    def get_news_article(self):
+        for article in self.parser.find_all('article'):
+            span = article.find('span', 'c-meta__type')
+            if span and span.string == 'News':
+                yield article
+
+    def get_article_body(self):
+        body = self.parser.find('div', 'c-article-body')
+        if body:
+            return body.get_text().strip()
+
+
+WebScrapper.scrape_url()
