9cf0cf7a8dcfae6b07b8533c5a6c71117da82667
Antonio Marí <amari@at4.net>
2021-09-29 20:05:04 +0200

    Stage 3/5

--- Web Scraper/task/scraper.py
+++ Web Scraper/task/scraper.py
@@ -1,43 +1,18 @@
 import requests
-from bs4 import BeautifulSoup
 
 
 class WebScrapper:
     @staticmethod
-    def check_url_quote(url):
+    def save_content_to_file(url):
         print()
-        try:
-            if "https://www.imdb.com/title/" not in url:
-                raise RuntimeError("Invalid movie page!")
-            response = requests.get(url, headers={'Accept-Language': 'en-US,en;q=0.5'})
-            if response:
-                movie = dict()
-                html_parser = HtmlParser(response.content)
-                movie['title'] = html_parser.get_title()
-                movie['description'] = html_parser.get_description()
-                print(movie)
-            else:
-                print("Invalid movie page!")
-        except RuntimeError as e:
-            print(str(e))
-
-
-class HtmlParser:
-    def __init__(self, html_content):
-        self.parser = BeautifulSoup(html_content, 'html.parser')
-
-    def get_title(self):
-        title = self.parser.find('title')
-        if title is None or not title.text.endswith(' - IMDb'):
-            raise RuntimeError("Invalid movie page!")
-        return title.text.replace(' - IMDb', '', 1)
-
-    def get_description(self):
-        description = self.parser.find('meta', {'name': 'description'})
-        if description is None:
-            raise RuntimeError("Invalid movie page!")
-        return description.get('content')
+        response = requests.get(url, headers={'Accept-Language': 'en-US,en;q=0.5'})
+        if response:
+            with open('source.html', 'wb') as source_file:
+                source_file.write(response.content)
+                print('Content saved.')
+        else:
+            print(f"The URL returned {response.status_code}")
 
 
 print("Input the URL:")
-WebScrapper.check_url_quote(input())
+WebScrapper.save_content_to_file(input())
